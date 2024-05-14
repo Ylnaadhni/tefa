70dfe1c68a3636c4c2def63a383ff9962e148d54
yulianaadhani <142281027+Ylnaadhni@users.noreply.github.com>
2024-05-14 10:16:20 +0700

    Add files via upload

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="row p-3 bg-primary text-white">
     <div class="col-lg-1">
-      <img src="~/assets/img/LOGO-SMK4 (1).png" alt="logo SMKN 4">
+      <img src="~/assets/img/logo.smk4.webp" alt="logo SMKN 4">
     </div>
 
     <div class="col-lg-11">
